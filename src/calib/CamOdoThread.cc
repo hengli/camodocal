69c5a22bcba6055771ff487c31709cefcbb8a930
Lionel Heng <hengli@student.ethz.ch>
2014-04-17 11:13:11 +0200

    Make calibration work with GPS/INS data in lieu of odometry data.

--- src/calib/CamOdoThread.cc
+++ src/calib/CamOdoThread.cc
@@ -329,12 +329,16 @@ CamOdoThread::threadFunction(void)
                 bool camValid = tracker.addFrame(frame, m_camera->mask());
 
                 // tag frame with odometry and GPS/INS data
-                frame->odometryMeasurement() = boost::make_shared<Odometry>();
-                *(frame->odometryMeasurement()) = *interpOdo;
-                frame->systemPose() = boost::make_shared<Odometry>();
-                *(frame->systemPose()) = *interpOdo;
 
-                if (interpGpsIns.get() != 0)
+                if (m_poseSource == GPS_INS)
+                {
+                    frame->odometryMeasurement() = boost::make_shared<Odometry>();
+                    *(frame->odometryMeasurement()) = *interpOdo;
+                    frame->systemPose() = boost::make_shared<Odometry>();
+                    *(frame->systemPose()) = *interpOdo;
+                }
+
+                if (interpGpsIns)
                 {
                     frame->gpsInsMeasurement() = interpGpsIns;
                 }
