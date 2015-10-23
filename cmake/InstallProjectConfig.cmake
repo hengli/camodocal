244db8293034e6a628571ce03f3e06b51fd8c7d3
Andrew Hundt <ATHundt@gmail.com>
2015-10-23 17:31:03 -0400

    cmake fixes for when camodocal is part of another project

--- cmake/InstallProjectConfig.cmake
+++ cmake/InstallProjectConfig.cmake
@@ -2,7 +2,7 @@
 
 include(CMakePackageConfigHelpers)
 configure_package_config_file(
-  ${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake.in
+  ${PROJECT_CMAKE_DIR}/${PROJECT_NAME}Config.cmake.in
   ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
   INSTALL_DESTINATION ${INSTALL_CMAKE_DIR}
   )
