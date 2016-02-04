6afef7c5f1ae0b6473efe328cf81dbfc9066372c
libo24 <libo24@yq01-idl-gpu-offline118.yq01.baidu.com>
2016-02-04 21:34:30 +0800

    compilation refactored. Removed custom FindOpenCV. Removed custom CMAKE_LIBRARY_PATH and CMAKE_INCLUDE_PATH

--- cmake/LibraryConfig.cmake
+++ cmake/LibraryConfig.cmake
@@ -84,7 +84,7 @@ endif()
 
 ##### Boost #####
 # Expand the next statement if newer boost versions than 1.40.0 are released
-set(Boost_ADDITIONAL_VERSIONS "1.40" "1.40.0" "1.49" "1.49.0")
+# set(Boost_ADDITIONAL_VERSIONS "1.40" "1.40.0" "1.49" "1.49.0")
 
 find_package(Boost 1.40 REQUIRED COMPONENTS filesystem program_options serialization system thread)
 
