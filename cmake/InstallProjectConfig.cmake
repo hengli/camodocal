4ec21006549015fe68cc82e8b68ecd15467ca940
Andrew Hundt <ATHundt@gmail.com>
2016-03-15 12:36:48 -0400

    CMake script now installs Find*.cmake scripts so dependent libraries can use them. Config script also checks for some essentials with find_dependency.

--- cmake/InstallProjectConfig.cmake
+++ cmake/InstallProjectConfig.cmake
@@ -16,3 +16,12 @@ install(
   DESTINATION ${INSTALL_CMAKE_DIR}
   COMPONENT Devel
   )
+
+export(PACKAGE ${PROJECT_NAME})
+
+
+###################### Add uninstall target ############################
+if("${CMAKE_PROJECT_NAME}" STREQUAL "${PROJECT_NAME}")
+    ADD_CUSTOM_TARGET(uninstall
+      COMMAND ${CMAKE_COMMAND} -P ${PROJECT_CMAKE_DIR}/cmake_uninstall.cmake)
+endif()
