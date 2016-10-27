9b135dd9aa0ea0788fecbbe4ad396df8a2a6f88c
Andrew Hundt <ATHundt@gmail.com>
2016-10-26 21:25:37 -0400

    Release Version 1.0.0 - PackageConfig.cmake works

--- cmake/InstallProjectConfig.cmake
+++ cmake/InstallProjectConfig.cmake
@@ -1,6 +1,16 @@
 ###################### InstallProjectConfig  ###########################
 
 include(CMakePackageConfigHelpers)
+
+# Configuration
+set(version_config "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake")
+
+# Configure '<PROJECT-NAME>ConfigVersion.cmake'
+# Note: PROJECT_VERSION is used as a VERSION
+write_basic_package_version_file(
+    "${version_config}" COMPATIBILITY SameMajorVersion
+)
+
 configure_package_config_file(
   ${PROJECT_CMAKE_DIR}/${PROJECT_NAME}Config.cmake.in
   ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
@@ -12,7 +22,9 @@ install(EXPORT ${PROJECT_NAME}Targets
   )
 
 install(
-  FILES ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+  FILES 
+  ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+  "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
   DESTINATION ${INSTALL_CMAKE_DIR}
   COMPONENT Devel
   )
